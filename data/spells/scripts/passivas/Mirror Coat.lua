bfe820e9d72cc54132d637cfc3bc81382642304c
CaosJunior <yagocaos@gmail.com>
2016-08-05 10:31:47 -0300

    Add files via upload

---
+++ data/spells/scripts/passivas/Mirror Coat.lua
@@ -0,0 +1,6 @@
+function onCastSpell(cid, var)
+
+docastspell(cid, "Mirror Coat")
+
+return true
+end
---
+++ data/spells/scripts/passivas/Stunning Confusion.lua
@@ -0,0 +1,6 @@
+function onCastSpell(cid, var)
+
+docastspell(cid, "Stunning Confusion")
+
+return true
+end
